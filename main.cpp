8502c5506f2a95ae62d5cf679ec053272bfe65ae
Alex Roberts <alecroberts92@gmail.com>
2022-12-27 11:44:27 +0000

    Call playBlackjack() function in main.cpp
    This calls the function in main and handles all the possible outputs.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
+#include "blackjack.h"
+
 #include <iostream>
 
 int main()
 {
+    Deck deck {createDeck()};
+    shuffleDeck(deck);
+    // Uncomment this function call for debugging (or cheating!)
+    // printDeck(deck);
+    Outcome result {playBlackjack(deck)};
+    switch (result)
+    {
+    case Outcome::Win:
+        std::cout << "You win!\n";
+        break;
+    case Outcome::Loss:
+        std::cout << "You lose.\n";
+        break;
+    case Outcome::Tie:
+        std::cout << "Result is a tie.\n";
+        break;
+    
+    default:
+        std::cout << "Undefined.\n";
+        break;
+    }
+
     return 0;
 }
